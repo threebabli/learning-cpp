0f303ba0cc70f12b48ba49e41202cfa77c36d151
threebabli <interact.babli@gmail.com>
2026-01-10 23:11:02 +0530

    add a file

---
+++ basic_calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "enter a number \n";
+    cin >> a;
+
+    cout << "enter another number \n";
+    cin >> b;
+
+    sum = a + b ;
+    cout << "the sum of those number is \n" << sum << endl;
+
+    return 0;
+
+}
